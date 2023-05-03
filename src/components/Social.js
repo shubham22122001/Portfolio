5c460f4f6bd105f7f5077faebc50a6ddb356385f
shubham22122001 <shubhambhadra10@gmail.com>
2023-05-03 14:26:51 +0530

    sixth commit

--- src/components/Social.js
+++ src/components/Social.js
@@ -7,7 +7,7 @@ const Social = () => {
                     
                     <div className='link'><a href='https://github.com/shubham22122001/'z target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a></div>
                     <div className='link'><a href='https://www.linkedin.com/in/shubham-bhadra-4a1b121b1/' target="_blank"  rel="noreferrer"><i className="fa-brands fa-linkedin-in"></i></a></div>
-                    <div className='link'><a href='public\Shubham_Resume_og.pdf' target="_blank"  rel="noreferrer" download={true}><i className="fa fa-file"></i></a></div>
+                    <div className='link'><a href='public\Shubham_Resume_og_new.pdf' target="_blank"  rel="noreferrer" download={true}><i className="fa fa-file"></i></a></div>
 
 
                 </div>
